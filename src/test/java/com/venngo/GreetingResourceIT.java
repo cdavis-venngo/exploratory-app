ada7ba4e06cd5d72f1ce50043ce544f53d0bd910
quarkusio <no-reply@quarkus.io>
2024-05-06 14:28:48 +0000

    Initial commit

---
+++ src/test/java/com/venngo/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.venngo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
